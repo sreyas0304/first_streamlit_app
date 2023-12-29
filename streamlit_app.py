f87124d79f7255f4afb86479ffeb88d28300d31d
sreyas0304 <55827902+sreyas0304@users.noreply.github.com>
2023-12-29 17:24:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,8 @@ streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_rows)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
 
-addfruit_response = requests.get("https://fruityvice.com/api/fruit/"+ add_my_fruit)
 
 
