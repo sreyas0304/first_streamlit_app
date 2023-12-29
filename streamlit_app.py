02120be94aaf61cffe156d3028b293476f9ecd65
sreyas0304 <55827902+sreyas0304@users.noreply.github.com>
2023-12-29 18:19:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -21,9 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-
-import requests
-
 streamlit.header('Fruityvice Fruit Advice!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
@@ -38,7 +38,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
